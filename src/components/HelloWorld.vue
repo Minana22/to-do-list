c5cfc59fb5eb04e0a335e4a2d807dfb332ad384f
Mina Milovic <mina.milovic@logit-solutions.com>
2023-01-27 01:30:50 +0100

    new color

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,70 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="main">
+    <div class="toDo">
+      <button @click="addTask(n)" class="btn" v-for="n in toDoniz" :key="n">
+        {{ n }}
+      </button>
+    </div>
+    <div class="push">
+      <button @click="removeTask(n)" class="btn" v-for="n in doneNiz" :key="n">
+        {{ n }}
+      </button>
+    </div>
   </div>
 </template>
-
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  data: function () {
+    return {
+      toDoniz: ["task 1", "task 2", "task 3", "task 4", "task 5", "task 6"],
+      doneNiz: [],
+    };
+  },
+  name: "HelloWorld",
+  methods: {
+    addTask(task) {
+      this.toDoniz = this.toDoniz.filter((t) => t !== task);
+      this.doneNiz.push(task);
+    },
+    removeTask(task) {
+      this.doneNiz = this.doneNiz.filter((t) => t !== task);
+      this.toDoniz.push(task);
+    },
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.main {
+  width: 100%;
+  height: 90vh;
+  background-color: rgb(21, 128, 124);
+  display: flex;
+  justify-content: center;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.btn {
+  width: 200px;
+  height: 80px;
+  margin: 10px;
+  background-color: rgb(154, 223, 218);
+  cursor: pointer;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.toDo {
+  border: 5px solid rgb(5, 83, 48);
+  width: 20%;
+  background-color: rgb(214, 172, 253);
+  display: flex;
+  flex-direction: column;
+  margin: 10px;
+  align-items: center;
 }
-a {
-  color: #42b983;
+.push {
+  border: 5px solid rgb(5, 83, 48);
+  width: 20%;
+  background-color: rgb(214, 172, 253);
+  display: flex;
+  flex-direction: column;
+  margin: 10px;
+  align-items: center;
 }
 </style>
